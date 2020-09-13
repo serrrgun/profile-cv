c2a62e1400390790ad61afece2db999593840534
serrrgun <serrrgun@mail.ru>
2020-09-13 19:06:26 +0300

    Update 2020-09-13T16:06:04.481Z

--- src/js/script.js
+++
@@ -1,9 +0,0 @@
-// menu
-
-var menuBtn = document.querySelector('.menu-btn');
-var header = document.querySelector('.header');
-
-menuBtn.addEventListener('click', function() {
-  menuBtn.classList.toggle('menu-btn--active');
-  header.classList.toggle('header--open');
-})
