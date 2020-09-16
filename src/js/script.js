6434b628aa648533e563f3a7d5437c30ec1321a2
serrrgun <serrrgun@mail.ru>
2020-09-16 17:01:26 +0300

    add template

--- src/js/script.js
+++ src/js/script.js
@@ -7,3 +7,21 @@ menuBtn.addEventListener('click', function() {
   menuBtn.classList.toggle('menu-btn--active');
   header.classList.toggle('header--open');
 })
+
+// slider
+
+var swiper = new Swiper('.swiper-container', {
+  slidesPerView: 1,
+      spaceBetween: 30,
+      keyboard: {
+        enabled: true,
+      },
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      },
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+});
